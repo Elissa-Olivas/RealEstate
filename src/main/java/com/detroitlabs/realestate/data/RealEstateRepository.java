d8e24a7b8ebbd9f09d8975dde8caaeed1ca498df
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-03-22 10:01:46 -0600

    Adding property details

--- src/main/java/com/detroitlabs/realestate/data/RealEstateRepository.java
+++ src/main/java/com/detroitlabs/realestate/data/RealEstateRepository.java
@@ -14,7 +14,8 @@ public class RealEstateRepository {
     private static final List<Properties> ALL_PROPERTIES = Arrays.asList(
             new Properties(419000, 2, 2, "Denver", "CO", "2685 S Dayton Way", 1938, "https://photos.zillowstatic.com/fp/a88dd6bfddc78b464515a37ca851c228-cc_ft_576.webp"),
             new Properties(425000, 3, 3, "Denver", "CO", "16087 E Elk Place", 1454, "https://photos.zillowstatic.com/fp/815111e6c5de00a7be0418228c81532d-cc_ft_576.webp"),
-            new Properties(479300, 3, 3, "Denver", "CO", "20000 E Mitchell Pl #3", 1522, "https://photos.zillowstatic.com/fp/3dfd96a831757f8ebd97213eeec13a5b-cc_ft_576.webp"));
+            new Properties(479300, 3, 3, "Denver", "CO", "20000 E Mitchell Pl #3", 1522, "https://photos.zillowstatic.com/fp/3dfd96a831757f8ebd97213eeec13a5b-cc_ft_576.webp"),
+            new Properties(290000, 2, 3, "Denver", "CO", "4160 E Warren Avenue Unit 4,", 1232, "https://photos.zillowstatic.com/fp/4d8846e005c21f28d1b54a309007a393-cc_ft_576.webp"));
 
 
 
